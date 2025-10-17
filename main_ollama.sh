98167ae1e99dec448b34a08ea77fc1854ac2a08d
Sujin Koo <165651053+sujin-koo@users.noreply.github.com>
2025-10-17 13:50:48 +0900

    Delete main_ollama.sh

--- main_ollama.sh
+++
@@ -1,7 +0,0 @@
-cd /path/to/customized-arxiv-daily
-python main.py --categories cs.CV cs.AI cs.CL cs.CR cs.LG \
-    --provider Ollama --model deepseek-r1:7b \
-    --smtp_server smtp.qq.com --smtp_port 465 \
-    --sender * --receiver * \
-    --sender_password * \
-    --save
